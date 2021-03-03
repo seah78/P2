19143be94d7c41c18de3187b951aa9402d629e18
seah78 <github@seah.pro>
2021-03-03 05:03:12 +0100

    Dictionnaire ok

--- scrape.py
+++ scrape.py
@@ -1,8 +1,7 @@
-
 import requests
 from bs4 import BeautifulSoup
 
-temp_url = 'http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html'
+temp_url = 'http://books.toscrape.com/catalogue/sharp-objects_997/'
 
 def request(url):
 	response = requests.get(url)
@@ -16,76 +15,43 @@ def request(url):
 
 
 def scrape_book(url):
-	book = dict()
+	book = {} # dict()
+	sub_book = {}
 	soup = BeautifulSoup(request(url), 'html.parser')
-	book['product_page_url']=url 									#product_page_url
-	book['title']=soup.find('article', 'product_page').h1.text 		#title
-
 	trs = soup.findAll('tr')
 	#[print(str(tr) + '\n\n') for tr in trs]
 	for tr in trs:
 		th = tr.find('th').text
 		td = tr.find('td').text
-		print(th + ' ' + td)
+		sub_book[th] = td # ajout des product information à un sous-dictionnaire
+		#print(th + ' ' + td)
+	links_a = []
+	lis = soup.find('ul', 'breadcrumb').findAll('li')
+	for li in lis:
+		if li.find('a'):
+			links_a.append(li.find('a').text)	
+			
 
+	#print(str(sub_book) + '\n\n')
+
+	book['product_page_url'] = url 											#product_page_url
+	book['upc'] = sub_book['UPC'] 											#universal_ product_code (upc)
+	book['title'] = soup.find('article', 'product_page').h1.text 			#title
+	book['price_including_tax'] = sub_book['Price (incl. tax)'] 			#price_including_tax
+	book['price_excluding_tax'] = sub_book['Price (excl. tax)']				#price_excluding_tax
+	book['number_available'] = sub_book['Availability'] 					#number_available
+	description = soup.find('article').find('p', recursive=False)
+	if description:
+		description = description.text
+	else:
+		description = ""
+	book['product_description'] = description								#product_description
+	book['category'] = links_a[2] #category
+	book['review_rating'] = sub_book['Number of reviews'] 					#review_rating
+	book['image_url'] = url + soup.find('img')['src'].replace('../../', '') #image_url
 
 	return book
 
-a = scrape_book(temp_url)
-print(a)
+dictionnary_book = scrape_book(temp_url)
+print(dictionnary_book)
 
-"""
-
-
-
-#universal_ product_code (upc)
-
-price_including_tax
-price_excluding_tax
-number_available
-	book['product_description']=soup.find('div', {'id': 'product_description'}).p.text #product_description
-category
-review_rating
-image_url
-"""
-
-
-
-
-"""
-<tr>
-<th>UPC</th><td>a22124811bfa8350</td>
-</tr>
-
-
-<tr>
-<th>Product Type</th><td>Books</td>
-</tr>
-
-
-<tr>
-<th>Price (excl. tax)</th><td>£45.17</td>
-</tr>
-
-
-<tr>
-<th>Price (incl. tax)</th><td>£45.17</td>
-</tr>
-
-
-<tr>
-<th>Tax</th><td>£0.00</td>
-</tr>
-
-
-<tr>
-<th>Availability</th>
-<td>In stock (19 available)</td>
-</tr>
-
-
-<tr>
-<th>Number of reviews</th>
-<td>0</td>
-</tr>
-"""
