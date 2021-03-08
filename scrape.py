5c1407c5094b8a399cddfab8d82bd09b4d00ba5b
seah78 <github@seah.pro>
2021-03-08 11:10:22 +0100

    Branche csv - insertion newline

--- scrape.py
+++ scrape.py
@@ -35,8 +35,8 @@ bookcsv.close
 bookcsv = open('books.csv', 'r')
 """
 
-with open('books.csv', 'w') as f:
-     f.write(dictionnary_book['product_page_url'] + ';' +  dictionnary_book['upc'] + ';' +  dictionnary_book['title'] + ';' +  dictionnary_book['price_including_tax'] + ';' +  dictionnary_book['price_excluding_tax'] + ';' +  dictionnary_book['number_available'] + ';' +   dictionnary_book['product_description'] + ';' +  dictionnary_book['category'] + ';' +  dictionnary_book['review_rating'] + ';' +  dictionnary_book['image_url'] + '\n')
+with open('books.csv', 'w', newline='') as f:
+     f.write(dictionnary_book['product_page_url'] + ';' +  dictionnary_book['upc'] + ';' +  dictionnary_book['title'] + ';' +  dictionnary_book['price_including_tax'] + ';' +  dictionnary_book['price_excluding_tax'] + ';' +  dictionnary_book['number_available'] + ';' +   dictionnary_book['product_description'] + ';' +  dictionnary_book['category'] + ';' +  dictionnary_book['review_rating'] + ';' +  dictionnary_book['image_url'] + '')
 
 
 
