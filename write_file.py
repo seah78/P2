b67b0ed630aac2349bad1213bd4d04a103c542a4
seah78 <githubseah.pro>
2021-03-13 19:00:17 +0100

    Mise à jour gitignore

--- write_file.py
+++ write_file.py
@@ -1,6 +1,8 @@
-with open("imageTitre.jpg", "b") as file:
-    file.write(request(url_image))
+def save_image(url_image, upc):
+	with open(f'{upc}.jpg', "b") as file:
+	    file.write(request(url_image))
 
+def
 with open('books.csv', 'a', newline='', encoding='utf-8') as f:
 	csv_writer = csv.DictWriter(f, dictionnary_book.keys(), delimiter=';', dialect='excel') 
 	csv_writer.writeheader()
