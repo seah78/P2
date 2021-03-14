8cf840898223c83a84f622c428e55e3c64915ec5
seah78 <github@seah.pro>
2021-03-14 10:53:50 +0100

    Gestion de conflit write_file.py

--- write_file.py
+++ write_file.py
@@ -2,13 +2,6 @@ def save_image(url_image, upc):
 	with open(f'{upc}.jpg', "b") as file:
 	    file.write(request(url_image))
 
-<<<<<<< HEAD
-def
-with open('books.csv', 'a', newline='', encoding='utf-8') as f:
-	csv_writer = csv.DictWriter(f, dictionnary_book.keys(), delimiter=';', dialect='excel') 
-	csv_writer.writeheader()
-	csv_writer.writerow(dictionnary_book)
-=======
 def write_csv(dictionnary_book):
 	category = dictionnary_book['category']
 	with open(f'{category}.csv', 'a', newline='', encoding='utf-8') as f:
@@ -19,4 +12,3 @@ def write_csv(dictionnary_book):
 			csv_writer = csv.DictWriter(f, dictionnary_book.keys(), delimiter=';', dialect='excel') 
 			csv_writer.writeheader()
 			csv_writer.writerow(dictionnary_book)
->>>>>>> fd33b86c507ab77c53057f59a65bcdd6f1fd552e
